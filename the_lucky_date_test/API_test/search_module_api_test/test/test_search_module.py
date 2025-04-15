dbfb27bd92e415cd6893146838de654044ca886a
bagu work <bagguvixwork@gmail.com>
2025-04-15 19:06:41 +0200

    logging update

--- the_lucky_date_test/API_test/search_module_api_test/test/test_search_module.py
+++ the_lucky_date_test/API_test/search_module_api_test/test/test_search_module.py
@@ -6,22 +6,16 @@ from test_data import test_data_positive, test_data_negative
 
 @pytest.mark.parametrize("params", test_data_positive)
 
-def test_search_positive(params, setup_logger):
+def test_search_positive(params):
     response = search_users(criteria=params[0], limit=params[1])
-    logger = setup_logger
-    response = search_users(criteria=params[0], limit=params[1])
-    logger.info(f"[POSITIVE] status={response.status_code}, data={params}")
     assert response.status_code == 200
     allure.dynamic.title("POSITIVE тест")
 
 
 @pytest.mark.parametrize("params", test_data_negative)
     
-def test_search_negative(params, setup_logger):
+def test_search_negative(params):
     response = search_users(criteria=params[0], limit=params[1])
-    logger = setup_logger
-    response = search_users(criteria=params[0], limit=params[1])
-    logger.info(f"[NEGATIVE] status={response.status_code}, data={params}")
     assert response.status_code == 400
     allure.dynamic.title("NEGATIVE тест")
 
